e5374ae7280ece3788f606608040ba9c27d62472
Pasha Rumkin <dev@rumk.in>
2018-05-07 22:53:55 +0300

    [src] Update actions behavior on the client and server

--- app/actions.js
+++ app/actions.js
@@ -1,27 +1,23 @@
 const imm = require('../lib/imm');
 
-const setTitle = (value) => {
-  if (typeof document === 'undefined') {
-    return;
-  }
-
-  const node = document.head.querySelector('title');
-  node.textContent = value;
-}
-
 module.exports = ({history} = {}) => ({
   setTitle: (value) => (state) => {
-    setTitle(value);
+    if (state.isClient) {
+      document.title = title;
+    }
 
     return imm.set(state, 'title', value);
   },
   pageGoto: (url) => (state) => {
-    // TODO Get whole path.
-    if (history.location.pathname !== url) {
-      history.push(url);
-      // TODO Scroll to top or to anchor.
-      window.scrollTo(0, 0);
+    if (state.isClient) {
+      // TODO Get whole path.
+      if (history.location.pathname !== url) {
+        history.push(url);
+        // TODO Scroll to top or to anchor.
+        window.scrollTo(0, 0);
+      }
     }
+
 
     return imm.set(state, 'url', url);
   },
