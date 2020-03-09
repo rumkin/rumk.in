8c28fef71632c29d3ee10560f714458ea0fe39a6
Pavel Rumkin <dev@rumk.in>
2020-03-09 07:04:12 +0300

    [util,dep] Removed @rollup/plugin-json

--- rollup.config.js
+++ rollup.config.js
@@ -4,15 +4,15 @@ import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs'
 import replace from 'rollup-plugin-replace'
 import resolve from 'rollup-plugin-node-resolve'
-import json from '@rollup/plugin-json'
 import strip from '@rollup/plugin-strip'
+
 import removeExport from './workbench/rollup/remove-export'
 
 const DEV = process.env.NODE_ENV === 'development'
 
 const output = (filepath = 'index.js') => path.resolve(__dirname, 'build', filepath)
-const onDev = (...values) => DEV ? values : []
-const onProd = (...values) => !DEV ? values : []
+const onDev = (...exec) => DEV ? (exec() || []) : []
+const onProd = (...exec) => !DEV ? (exec() || []) : []
 
 const babelConfig = {
 	babelrc: false,
@@ -20,7 +20,7 @@ const babelConfig = {
 		['@babel/preset-react', {
 			pragma: 'h',
 		}],
-		...onProd(['minify', {
+		...onProd(() => ['minify', {
 			mangle: false,
 		}]),
 	],
@@ -48,12 +48,11 @@ export default [
 			}),
 			babel(babelConfig),
 			commonjs(),
-			json(),
 			replace({
 	      // Always production for React and ReactDOM packages
 	      'process.env.NODE_ENV': JSON.stringify('production'),
 	    }),
-			...onProd(strip({
+			...onProd(() => strip({
 				functions: [
 					'console.*',
 				]
